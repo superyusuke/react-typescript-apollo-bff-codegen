3bd9793bc0b16eca209378611bcc131a0559cf01
Nakanishi, Yusuke <nakanishiyusuke0000@gmail.com>
2020-06-27 12:24:08 +0900

    不要

--- server/src/graphModules/common/index.ts
+++ server/src/graphModules/common/index.ts
@@ -1,12 +1,7 @@
 import { gql } from "apollo-server-express";
 import { Resolvers } from "src/types/generated/graphql";
 import delay from "delay";
-// import { PrismaClient } from "src/generated/prismaClient";
-//
-// const prisma = new PrismaClient();
 
-import { Client } from "pg";
-import { callbackify } from "util";
 
 export const typeDefs = gql`
   scalar Upload
@@ -30,31 +25,5 @@ export const resolvers: Resolvers = {
       await delay(1000);
       return "this is health check message. OK";
     },
-    pg: async () => {
-      const client = new Client({
-        user: "postgres",
-        host: "/cloudsql/elegant-hope-273402:asia-northeast1:my-pgdb",
-        database: "postgres",
-        password: "test",
-        // port: 3211,
-      });
-
-      await client.connect();
-
-      const res = await client.query("SELECT * from account");
-      const item = res.rows[0].username;
-      return item;
-    },
-    // prismaDB: async () => {
-    //   console.log("prisma db start");
-    //   try {
-    //     const allUsers = await prisma.account.findMany();
-    //     console.log(allUsers, "allUsers");
-    //     return allUsers[0].username;
-    //   } catch (e) {
-    //     console.log(e.message);
-    //     throw new Error(e);
-    //   }
-    // },
   },
 };
